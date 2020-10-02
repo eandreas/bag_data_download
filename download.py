7e39609cc3cd0437c0af0691539a4b1aed474188
Andreas Dorta <eichi@gmx.ch>
2020-10-02 22:47:26 +0200

    added download for test data and case data

--- download.py
+++ download.py
@@ -5,7 +5,8 @@ import filecmp
 
 URLs = {
     'BAG_test_data': 'https://www.bag.admin.ch/dam/bag/de/dokumente/mt/k-und-i/aktuelle-ausbrueche-pandemien/2019-nCoV/covid-19-basisdaten-labortests.xlsx.download.xlsx/Dashboard_3_COVID19_labtests_positivity.xlsx',
-    'BAG_report_data': 'https://www.bag.admin.ch/dam/bag/de/dokumente/mt/k-und-i/aktuelle-ausbrueche-pandemien/2019-nCoV/covid-19-datengrundlage-lagebericht.xlsx.download.xlsx/200325_Datengrundlage_Grafiken_COVID-19-Bericht.xlsx'
+    'BAG_report_data': 'https://www.bag.admin.ch/dam/bag/de/dokumente/mt/k-und-i/aktuelle-ausbrueche-pandemien/2019-nCoV/covid-19-datengrundlage-lagebericht.xlsx.download.xlsx/200325_Datengrundlage_Grafiken_COVID-19-Bericht.xlsx',
+    'BAG_cases_data': 'https://www.bag.admin.ch/dam/bag/de/dokumente/mt/k-und-i/aktuelle-ausbrueche-pandemien/2019-nCoV/covid-19-basisdaten-fallzahlen.xlsx.download.xlsx/Dashboards_1&2_COVID19_swiss_data_pv.xlsx'
 }
 
 def download_file(url, path = '.', fn = None, overwrite = False):
@@ -19,34 +20,33 @@ def download_file(url, path = '.', fn = None, overwrite = False):
     r = requests.get(url, allow_redirects=True)
     open(path + '/' + fn, 'wb').write(r.content)
 
-url = URLs['BAG_report_data']
-target_dir = 'downloads/report_data/'
-
-# list of all files within target_dir
-ls = [f for f in os.listdir(target_dir) if os.path.isfile(os.path.join(target_dir, f))]
-
-# download the current file from bag
-fn = target_dir + 'xxx_' + url.split('/')[-1]
-download_file(URLs['BAG_report_data'], fn = fn, overwrite = True)
-
-# remembers if the downloaded file should be kept, initially keep = False
-keep = False
-
-if (len(ls) < 1):
-    # keep the downloaded file if it is the only one
-    keep = True
-else:
-    # compare the downloaded file to the previous one and keep if not identical
-    latest = target_dir + sorted(ls)[-1]
-    print(f'comparing {latest} and {fn}')
-    if not filecmp.cmp(latest, fn, shallow = False):
+def download_and_keep_if_new(url, target_dir):   
+    # list of all files within target_dir
+    ls = [f for f in os.listdir(target_dir) if os.path.isfile(os.path.join(target_dir, f))]
+    # download the current file from bag
+    fn = target_dir + 'xxx_' + url.split('/')[-1]
+    download_file(url, fn = fn, overwrite = True)
+    # remembers if the downloaded file should be kept, initially keep = False
+    keep = False
+    if (len(ls) < 1):
+        # keep the downloaded file if it is the only one
         keep = True
+    else:
+        # compare the downloaded file to the previous one and keep if not identical
+        latest = target_dir + sorted(ls)[-1]
+        print(f'comparing {latest} and {fn}')
+        if not filecmp.cmp(latest, fn, shallow = False):
+            keep = True
+    if keep:
+        # rename and keep the downloaded file
+        date_time = datetime.now().strftime("%Y-%m-%d_%H-%M")
+        fn_new = target_dir + date_time + '_' + url.split('/')[-1]
+        os.rename(fn, fn_new)
+        print(f'Keeping new file {fn_new}')
+    else:
+        # delete the downloaded file
+        os.remove(fn)
         
-if keep:
-    # rename and keep the downloaded file
-    date_time = datetime.now().strftime("%Y-%m-%d_%H-%M")
-    fn_new = target_dir + '/' + date_time + '_' + url.split('/')[-1]
-    os.rename(fn, fn_new)
-else:
-    # delete the downloaded file
-    os.remove(fn)
+download_and_keep_if_new(URLs['BAG_report_data'], 'downloads/report_data/')
+download_and_keep_if_new(URLs['BAG_test_data'], 'downloads/test_data/')
+download_and_keep_if_new(URLs['BAG_cases_data'], 'downloads/cases_data/')

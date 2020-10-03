817f3feac1593671fdbf958aef42de4c1af6ac31
Andreas Dorta <eichi@gmx.ch>
2020-10-03 21:40:58 +0200

    using pathlib.Path instead of os

--- download.py
+++ download.py
@@ -1,7 +1,7 @@
 import requests
-import os.path
-from datetime import datetime
 import filecmp
+from pathlib import Path
+from datetime import datetime
 
 URLs = {
     'BAG_test_data': 'https://www.bag.admin.ch/dam/bag/de/dokumente/mt/k-und-i/aktuelle-ausbrueche-pandemien/2019-nCoV/covid-19-basisdaten-labortests.xlsx.download.xlsx/Dashboard_3_COVID19_labtests_positivity.xlsx',
@@ -9,44 +9,53 @@ URLs = {
     'BAG_cases_data': 'https://www.bag.admin.ch/dam/bag/de/dokumente/mt/k-und-i/aktuelle-ausbrueche-pandemien/2019-nCoV/covid-19-basisdaten-fallzahlen.xlsx.download.xlsx/Dashboards_1&2_COVID19_swiss_data_pv.xlsx'
 }
 
-def download_file(url, path = '.', fn = None, overwrite = False):
-    # get the file name from url if not specified by fn argument
-    if fn == None:
-        fn = url.split('/')[-1]
-    # exit if file already exists and overwrite = False
-    if ((overwrite == False) and (os.path.isfile(path + '/' + fn))):
+
+def download(url, target_dir = Path.cwd(), file_name = None, overwrite = False):
+    '''
+    Downloads a file from an url into target_dir. If no file_name is probided, the file is named
+    as defined by the url. In case there is already a file named file_name within target_dir, overwrite=True
+    needs to be set to force saving the download.
+    '''
+    # get the file name from url if fn is None
+    if file_name is None:
+        file_name = url.split('/')[-1]
+    # exit if the file already exists and overwrite = False
+    f = target_dir / file_name
+    if (f.exists() and not overwrite):
         return
     # download and save the file
     r = requests.get(url, allow_redirects=True)
-    open(path + '/' + fn, 'wb').write(r.content)
+    open(f, 'wb').write(r.content)
+    return f
 
-def download_and_keep_if_new(url, target_dir):   
-    # list of all files within target_dir
-    ls = [f for f in os.listdir(target_dir) if os.path.isfile(os.path.join(target_dir, f))]
+    
+def download_if_new(url, target_dir, suffix = ''):
+    '''
+    Downloads a file fro url and stores it in target_dir unless there is already a file with
+    the same content (byte-by-byte comparison).
+    '''
+    # get the last modified file
+    try:
+        time, latest = max((f.stat().st_mtime, f) for f in target_dir.glob('*' + suffix))
+    except ValueError as e:
+        latest = None
     # download the current file from bag
-    fn = target_dir + 'xxx_' + url.split('/')[-1]
-    download_file(url, fn = fn, overwrite = True)
-    # remembers if the downloaded file should be kept, initially keep = False
-    keep = False
-    if (len(ls) < 1):
-        # keep the downloaded file if it is the only one
-        keep = True
+    f_download = download(url, target_dir, file_name = 'tmp', overwrite = True)
+    
+    # compare the latest file with the current download
+    if (latest is None):
+        same = False
     else:
-        # compare the downloaded file to the previous one and keep if not identical
-        latest = target_dir + sorted(ls)[-1]
-        print(f'comparing {latest} and {fn}')
-        if not filecmp.cmp(latest, fn, shallow = False):
-            keep = True
-    if keep:
-        # rename and keep the downloaded file
-        date_time = datetime.now().strftime("%Y-%m-%d_%H-%M")
-        fn_new = target_dir + date_time + '_' + url.split('/')[-1]
-        os.rename(fn, fn_new)
-        print(f'Keeping new file {fn_new}')
+        same = filecmp.cmp(str(latest), str(f_download), shallow = False)
+    
+    # rename or remove the current download if defferent from the previous file
+    if not same:
+        prefix = datetime.now().strftime("%Y-%m-%d_%H-%M")
+        f_new = target_dir / (prefix + '_' + url.split('/')[-1])
+        f_download.replace(f_new)
     else:
-        # delete the downloaded file
-        os.remove(fn)
-        
-download_and_keep_if_new(URLs['BAG_report_data'], 'downloads/report_data/')
-download_and_keep_if_new(URLs['BAG_test_data'], 'downloads/test_data/')
-download_and_keep_if_new(URLs['BAG_cases_data'], 'downloads/cases_data/')
+        f_download.unlink()
+    
+download_if_new(URLs['BAG_report_data'], Path('downloads/report_data'), suffix = '.xlsx')
+download_if_new(URLs['BAG_test_data'], Path('downloads/test_data'), suffix = '.xlsx')
+download_if_new(URLs['BAG_cases_data'], Path('downloads/cases_data'), suffix = '.xlsx')

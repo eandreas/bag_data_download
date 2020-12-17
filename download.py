6120e9dda50a1aa67560be390fbbb6aa7880e26d
Andreas Dorta <eichi@gmx.ch>
2020-12-17 21:57:35 +0100

    Get current url for csv download from bag website

--- download.py
+++ download.py
@@ -2,14 +2,15 @@ import requests
 import filecmp
 from pathlib import Path
 from datetime import datetime
+from bs4 import BeautifulSoup
 
 URLs = {
     'BAG_test_data': 'https://www.bag.admin.ch/dam/bag/de/dokumente/mt/k-und-i/aktuelle-ausbrueche-pandemien/2019-nCoV/covid-19-basisdaten-labortests.xlsx.download.xlsx/Dashboard_3_COVID19_labtests_positivity.xlsx',
     'BAG_report_data': 'https://www.bag.admin.ch/dam/bag/de/dokumente/mt/k-und-i/aktuelle-ausbrueche-pandemien/2019-nCoV/covid-19-datengrundlage-lagebericht.xlsx.download.xlsx/200325_Datengrundlage_Grafiken_COVID-19-Bericht.xlsx',
     'BAG_cases_data': 'https://www.bag.admin.ch/dam/bag/de/dokumente/mt/k-und-i/aktuelle-ausbrueche-pandemien/2019-nCoV/covid-19-basisdaten-fallzahlen.xlsx.download.xlsx/Dashboards_1&2_COVID19_swiss_data_pv.xlsx',
-    'BAG_csv_data': 'https://www.covid19.admin.ch/api/data/20201106-n2t4sx0k/downloads/sources-csv.zip'
+    'BAG_covid19_website': 'https://www.covid19.admin.ch'
+    
 }
-
 
 def download(url, target_dir = Path.cwd(), file_name = None, overwrite = False):
     '''
@@ -56,8 +57,31 @@ def download_if_new(url, target_dir, suffix = ''):
         f_download.replace(f_new)
     else:
         f_download.unlink()
+        
+def get_csv_url(website, append_to_website = False):  
+    page = requests.get(website)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    # Extract and store in top_items according to instructions on the left
+    links = soup.select('a')
+    url = None
+    for link in soup.select('a'):
+        text = link.text
+        text = text.strip() if text is not None else ''
+        if (text == 'Daten als .csv'):
+            url = link.get('href')
+            url = url.strip() if url is not None else ''
+            break
     
+    if (append_to_website):
+        url = website + url
+
+    return url
+
+# download new data
 download_if_new(URLs['BAG_report_data'], Path('downloads/report_data'), suffix = '.xlsx')
 download_if_new(URLs['BAG_test_data'], Path('downloads/test_data'), suffix = '.xlsx')
 download_if_new(URLs['BAG_cases_data'], Path('downloads/cases_data'), suffix = '.xlsx')
-download_if_new(URLs['BAG_csv_data'], Path('downloads/csv_data'), suffix = '.zip')
+
+csv_url = get_csv_url(URLs['BAG_covid19_website'], append_to_website = True)
+download_if_new(csv_url, Path('downloads/csv_data'), suffix = '.zip')

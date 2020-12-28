06bcb73e0b8ae683dd7f433dca33b038104a18f0
Andreas Dorta <eichi@gmx.ch>
2020-12-28 22:24:13 +0100

    Add daily reports for quartals

--- download.py
+++ download.py
@@ -8,8 +8,11 @@ URLs = {
     'BAG_test_data': 'https://www.bag.admin.ch/dam/bag/de/dokumente/mt/k-und-i/aktuelle-ausbrueche-pandemien/2019-nCoV/covid-19-basisdaten-labortests.xlsx.download.xlsx/Dashboard_3_COVID19_labtests_positivity.xlsx',
     'BAG_report_data': 'https://www.bag.admin.ch/dam/bag/de/dokumente/mt/k-und-i/aktuelle-ausbrueche-pandemien/2019-nCoV/covid-19-datengrundlage-lagebericht.xlsx.download.xlsx/200325_Datengrundlage_Grafiken_COVID-19-Bericht.xlsx',
     'BAG_cases_data': 'https://www.bag.admin.ch/dam/bag/de/dokumente/mt/k-und-i/aktuelle-ausbrueche-pandemien/2019-nCoV/covid-19-basisdaten-fallzahlen.xlsx.download.xlsx/Dashboards_1&2_COVID19_swiss_data_pv.xlsx',
-    'BAG_covid19_website': 'https://www.covid19.admin.ch'
-    
+    'BAG_covid19_website': 'https://www.covid19.admin.ch',
+    'BAG_2020_Q1': 'https://www.bag.admin.ch/dam/bag/de/dokumente/mt/k-und-i/aktuelle-ausbrueche-pandemien/2019-nCoV/bisherige-lageberichte-q1-2020.zip.download.zip/Lageberichte_Quartal_1_2020_DE.zip',
+    'BAG_2020_Q2': 'https://www.bag.admin.ch/dam/bag/de/dokumente/mt/k-und-i/aktuelle-ausbrueche-pandemien/2019-nCoV/bisherige-lageberichte-q2-2020.zip.download.zip/Lageberichte_Quartal_2_2020_DE.zip',
+    'BAG_2020_Q3': 'https://www.bag.admin.ch/dam/bag/de/dokumente/mt/k-und-i/aktuelle-ausbrueche-pandemien/2019-nCoV/bisherige-lageberichte-q3-2020.zip.download.zip/Lageberichte_Quartal_3_2020_DE.zip',
+    'BAG_2020_Q4': 'https://www.bag.admin.ch/dam/bag/de/dokumente/mt/k-und-i/aktuelle-ausbrueche-pandemien/2019-nCoV/bisherige-lageberichte-q4-2020.zip.download.zip/Lageberichte_Quartal_4_2020_DE.zip'
 }
 
 def download(url, target_dir = Path.cwd(), file_name = None, overwrite = False):
@@ -78,13 +81,35 @@ def get_link_url(website, link_name, append_to_website = False):
 
     return url
 
+
+# data directory definitions
+daily_reports_dir = Path('downloads/daily_reports')
+report_data_dir = Path('downloads/report_data')
+test_data_dir = Path('downloads/test_data')
+cases_data_dir = Path('downloads/cases_data')
+csv_data_dir = Path('downloads/csv_data')
+json_data_dir = Path('downloads/json_data')
+
+# create data directories
+Path.mkdir(daily_reports_dir, exist_ok = True)
+Path.mkdir(report_data_dir, exist_ok = True)
+Path.mkdir(test_data_dir, exist_ok = True)
+Path.mkdir(cases_data_dir, exist_ok = True)
+Path.mkdir(csv_data_dir, exist_ok = True)
+Path.mkdir(json_data_dir, exist_ok = True)
+
 # download new data
-download_if_new(URLs['BAG_report_data'], Path('downloads/report_data'), suffix = '.xlsx')
-download_if_new(URLs['BAG_test_data'], Path('downloads/test_data'), suffix = '.xlsx')
-download_if_new(URLs['BAG_cases_data'], Path('downloads/cases_data'), suffix = '.xlsx')
-
+download_if_new(URLs['BAG_2020_Q1'], daily_reports_dir, suffix = '.zip')
+download_if_new(URLs['BAG_2020_Q2'], daily_reports_dir, suffix = '.zip')
+download_if_new(URLs['BAG_2020_Q3'], daily_reports_dir, suffix = '.zip')
+download_if_new(URLs['BAG_2020_Q4'], daily_reports_dir, suffix = '.zip')
+#
+download_if_new(URLs['BAG_report_data'], report_data_dir, suffix = '.xlsx')
+download_if_new(URLs['BAG_test_data'], test_data_dir, suffix = '.xlsx')
+download_if_new(URLs['BAG_cases_data'], cases_data_dir, suffix = '.xlsx')
+#
 csv_url = get_link_url(URLs['BAG_covid19_website'], link_name = 'Daten als .csv', append_to_website = True)
-download_if_new(csv_url, Path('downloads/csv_data'), suffix = '.zip')
-
+download_if_new(csv_url, csv_data_dir, suffix = '.zip')
+#
 json_url = get_link_url(URLs['BAG_covid19_website'], link_name = 'Daten als .json', append_to_website = True)
-download_if_new(json_url, Path('downloads/json_data'), suffix = '.zip')
+download_if_new(json_url, json_data_dir, suffix = '.zip')
